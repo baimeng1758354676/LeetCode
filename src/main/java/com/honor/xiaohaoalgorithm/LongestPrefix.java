47eac247e9dc7a38996f98728456cfb33de8857e
mengzi <1758354676@qq.com>
2021-08-05 16:01:40 +0800

    最长公共前缀

---
+++ src/main/java/com/honor/xiaohaoalgorithm/LongestPrefix.java
@@ -0,0 +1,28 @@
+package com.honor.xiaohaoalgorithm;
+
+/**
+ * @Description 题目14: 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，则返回""
+ * 示例1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释:
+ * <p>
+ * 输入不存在公共前缀。
+ * 说明：
+ * <p>
+ * 所有输入只包含小写字母 a-z
+ * @Author baimeng
+ * @Date 2021/8/5 16:00
+ **/
+public class LongestPrefix {
+    public static void main(String[] args) {
+
+    }
+
+}
