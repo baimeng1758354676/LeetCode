ff3cda484ac14035e4fb4102535d56c80b25a6de
bm_bai <bm_bai@kingdee.com>
2021-10-10 22:44:43 +0800

    三数之和，排序，固定第一个数，双指针移动

---
+++ src/main/java/com/honor/xiaohaoalgorithm/SumOfThreeNum.java
@@ -0,0 +1,32 @@
+package com.honor.xiaohaoalgorithm;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。注意：答案中不可以包含重复的三元组。
+ * 示例：
+ * <p>
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ *
+ * @program: cloud-integration
+ * @description:
+ * @Author: baimeng
+ * @Date: 2021/10/10 22:32
+ */
+public class SumOfThreeNum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] sumOfThree(int[] nums) {
+        Arrays.sort(nums);
+
+        return null;
+    }
+}
